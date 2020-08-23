39af3ecbe3f289aac8e25b0903364e8b7f8232fc
Henrik Nørgaard <menneskefernis@gmail.com>
2020-08-24 00:12:32 +0200

    Create navigation bar

---
+++ main.js
@@ -0,0 +1,15 @@
+const navItems = document.querySelectorAll('.main-item');
+
+const toggleNav = (e) => {
+  const classList = e.target.classList;
+  classList.contains('open') ? null : closeAll();
+  classList.toggle('open');
+};
+
+const closeAll = () => {
+  navItems.forEach((item) => {
+    item.classList.remove('open');
+  });
+};
+
+navItems.forEach((item) => item.addEventListener('click', toggleNav));
