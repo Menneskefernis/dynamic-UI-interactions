2c4710aa7109a8d00ad232318cdac272e43697c9
Henrik Nørgaard <menneskefernis@gmail.com>
2020-08-24 00:16:19 +0200

    Prettify code

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const navItems = document.querySelectorAll('.main-item');
+const navItems = document.querySelectorAll(".main-item");
 
 const toggleNav = (e) => {
   const classList = e.target.classList;
-  classList.contains('open') ? null : closeAll();
-  classList.toggle('open');
+  classList.contains("open") ? null : closeAll();
+  classList.toggle("open");
 };
 
 const closeAll = () => {
   navItems.forEach((item) => {
-    item.classList.remove('open');
+    item.classList.remove("open");
   });
 };
 
-navItems.forEach((item) => item.addEventListener('click', toggleNav));
+navItems.forEach((item) => item.addEventListener("click", toggleNav));
