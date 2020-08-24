c4224e2047215b9a10f4928b1d58c2f632b4ebd8
Henrik Nørgaard <menneskefernis@gmail.com>
2020-08-25 00:57:55 +0200

    Finish mobile nav

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
+const mainNav = document.getElementById('main');
 const navItems = document.querySelectorAll(".main-item");
 const burgerMenuBtn = document.getElementById('burger-menu-btn');
 
 const toggleNav = (e) => {
   const classList = e.target.classList;
-  classList.contains("open") ? null : closeAll();
-  classList.toggle("open");
+  classList.contains('open') ? null : closeAll();
+  classList.toggle('open');
 };
 
 const closeAll = () => {
   navItems.forEach((item) => {
-    item.classList.remove("open");
+    item.classList.remove('open');
   });
 };
 
 const unfoldBurgerMenu = () => {
-  console.log('john')
+  mainNav.classList.toggle('open');
 };
 
-navItems.forEach((item) => item.addEventListener("click", toggleNav));
+navItems.forEach((item) => item.addEventListener('click', toggleNav));
 burgerMenuBtn.addEventListener('click', unfoldBurgerMenu);
