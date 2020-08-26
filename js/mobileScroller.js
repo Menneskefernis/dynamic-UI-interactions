393dd4a0b6e7b163404933f58d2b1be53eb9666a
Henrik Nørgaard <menneskefernis@gmail.com>
2020-08-26 17:31:26 +0200

    Make right caret work (mostly)

--- js/mobileScroller.js
+++ js/mobileScroller.js
@@ -2,14 +2,20 @@ const linksList = document.getElementById('nav-items');
 const caretLeft = document.getElementById('caret-left');
 const caretRight = document.getElementById('caret-right');
 
-console.log(linksList.scrollWidth);
+const items = Array.from(linksList.children);
 
+const getElementMargins = () => {
+    const margin = getComputedStyle(items[0]).margin.split("px")[0];
+    return margin * 2;
+};
+
+const scrollerWidth = items.reduce((totalWidth, item) => totalWidth + (item.getBoundingClientRect().width + getElementMargins()), 0);
+const halfItemWidth = (scrollerWidth / items.length) / 2;
+const endPadding = getComputedStyle(items[items.length - 1]).paddingRight;
 
 const handleCarets = () => {
-    linksList.scrollLeft > 60 ? caretLeft.classList.add('visible') : caretLeft.classList.remove('visible');
-    console.log(linksList.scrollWidth)
-    //(linksList.getBoundingClientRect().width - window.innerWidth - 1) > linksList.scrollLeft ? caretRight.classList.add('visible') : caretRight.classList.remove('visible');
-   
+    linksList.scrollLeft > halfItemWidth ? caretLeft.classList.add('visible') : caretLeft.classList.remove('visible');
+    (scrollerWidth - linksList.clientWidth - halfItemWidth - endPadding) > linksList.scrollLeft ? caretRight.classList.add('visible') : caretRight.classList.remove('visible');
 };
 
 linksList.addEventListener('scroll', handleCarets, {passive: true});
