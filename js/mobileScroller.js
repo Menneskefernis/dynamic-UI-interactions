041e886647d1e84f258e594ccfe78d6f86667ef4
Henrik Nørgaard <menneskefernis@gmail.com>
2020-08-26 20:09:34 +0200

    Finish mobile nav scroller

--- js/mobileScroller.js
+++ js/mobileScroller.js
@@ -2,20 +2,17 @@ const linksList = document.getElementById('nav-items');
 const caretLeft = document.getElementById('caret-left');
 const caretRight = document.getElementById('caret-right');
 
-const items = Array.from(linksList.children);
-
-const getElementMargins = () => {
-    const margin = getComputedStyle(items[0]).margin.split("px")[0];
-    return margin * 2;
+const getItemWidth = () => {
+    const items = Array.from(linksList.children);
+    const itemWidth = items[0].getBoundingClientRect().width;
+    return itemWidth;
 };
 
-const scrollerWidth = items.reduce((totalWidth, item) => totalWidth + (item.getBoundingClientRect().width + getElementMargins()), 0);
-const halfItemWidth = (scrollerWidth / items.length) / 2;
-const endPadding = getComputedStyle(items[items.length - 1]).paddingRight;
-
 const handleCarets = () => {
-    linksList.scrollLeft > halfItemWidth ? caretLeft.classList.add('visible') : caretLeft.classList.remove('visible');
-    (scrollerWidth - linksList.clientWidth - halfItemWidth - endPadding) > linksList.scrollLeft ? caretRight.classList.add('visible') : caretRight.classList.remove('visible');
+    linksList.scrollLeft > getItemWidth() ? caretLeft.classList.add('visible') : caretLeft.classList.remove('visible');
+    ((linksList.scrollWidth - linksList.clientWidth) - getItemWidth()) > linksList.scrollLeft
+        ? caretRight.classList.add('visible')
+        : caretRight.classList.remove('visible');
 };
 
 linksList.addEventListener('scroll', handleCarets, {passive: true});
